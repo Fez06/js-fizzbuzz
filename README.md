4d2b2a46299710d04b70d7a42f23417e61edf268
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-24 02:04:41 +0100

    bonus 1 e 2 fatti

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 -Faccio lo stesso con le iterazioni che danno modulo 0 del loro numero diviso 5.
 
 -Poi faccio la stessa cosa per quelli danno 0 sia diviso 3 che 5 e dico che console.log per loro sara' (fizzbuzz).[peccato che o non possa farlo perche' essendo js svolto dall'alto verso il basso una volta che assegna il primo e il secondo console.log ad una iterazione se lo terra', quindi cambio l'ordine e dico prima il console.log di entrambi e poi i singoli.]
+
--- main.js
+++ main.js
@@ -2,19 +2,37 @@
 
 const ul = document.querySelector('ul.list');
 
+
 for (let i = 1; i <= 100; i++){
     
+    const li = document.createElement('li');
+
     if (i % 5 === 0 && i % 3 === 0) {
-        console.log('fizzbuzz');
+        //console.log('fizzbuzz');
+        li.append('fizzbuzz');
+        ul.append(li);
+        li.classList.add('square', 'fizzbuzz');
     }
     else if (i % 5 === 0) {
-        console.log('buzz');
+        //console.log('buzz');
+        li.append('buzz');
+        ul.append(li);
+        li.classList.add('square', 'buzz');
     } 
     else if (i % 3 === 0) {
-        console.log('fizz');
+        //console.log('fizz');
+        li.append('fizz');
+        ul.append(li);
+        li.classList.add('square', 'fizz');
     }
     else {
-        console.log(i);
+        //console.log(i);
+        li.append(i);
+        ul.append(li);
+        li.classList.add('square', 'normal');
     }
 
+
+    
+    
 };
